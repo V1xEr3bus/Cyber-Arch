6d9525032bbec351dcd64a3dc9fc09703a94b2ec
Anubis <levsyev@gmail.com>
2025-08-16 15:13:28 +0300

    Made the wallpaper change notification self disapearing

--- config/hypr/scripts/toggle_wallpaper_mode.sh
+++ config/hypr/scripts/toggle_wallpaper_mode.sh
@@ -8,6 +8,15 @@
 MODE_FILE="$HOME/.config/waypaper/mode"
 current=$(cat "$MODE_FILE")
 
+notify() {
+  notify-send \
+    -a "Wallpaper Mode" \
+    -u low \
+    -h string:x-canonical-private-synchronous:wallpaper-mode \
+    -h boolean:transient:true \
+    "$1"
+}
+
 if [[ "$current" == "normal" ]]; then
     echo "nsfw" > "$MODE_FILE"
     notify-send "Wallpaper mode: NSFW 🫣"
